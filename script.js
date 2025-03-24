a4400b9dd46f676789dc7dfb228ecc203ad3fc02
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-03-23 23:30:34 -0300

    Adiciona uma nova div para o checkbox e o texto e também o justify between para o X ficar longe do texto

--- script.js
+++ script.js
@@ -27,7 +27,11 @@ addTaskButton.addEventListener('click', () => {
     if (taskText !== "") {
         // cria nova tarefa
         const taskElement = document.createElement('div');
-        taskElement.classList.add('flex', 'items-center', 'space-x-2', 'mb-2');
+        taskElement.classList.add('flex', 'items-center', 'space-x-2', 'mb-2', 'justify-between');
+
+        // cria container para o texto e checkbox
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('flex', 'items-center', 'space-x-2'); 
 
         // cria checkbox
         const checkbox = document.createElement('input');
@@ -39,9 +43,17 @@ addTaskButton.addEventListener('click', () => {
         taskTextElement.classList.add('text-gray-800', 'font-medium', 'dark:text-gray-300');
         taskTextElement.textContent = taskText;
 
+        // Cria o botão de exclusão
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'X';
+        deleteButton.classList.add('text-red-500', 'hover:text-red-800', 'ml-2', 'font-bold', 'cursor-pointer');
+
         // Adiciona um elemento dentro do outro
-        taskElement.appendChild(checkbox);
-        taskElement.appendChild(taskTextElement);
+        textContainer.appendChild(checkbox);
+        textContainer.appendChild(taskTextElement);
+        
+        taskElement.appendChild(textContainer);
+        taskElement.appendChild(deleteButton); 
         todoList.appendChild(taskElement);
 
         // LIMPA O INPUT
@@ -54,6 +66,10 @@ addTaskButton.addEventListener('click', () => {
             } else {
                 taskTextElement.classList.remove('line-through', 'text-gray-400');
             }
+        });
+
+        deleteButton.addEventListener('click', () => {
+            taskElement.remove(); // Remove a tarefa do DOM
         });
 
     }
